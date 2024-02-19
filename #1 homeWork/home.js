5b0c3bd840e21e65bfd5d9ae88c6a1cad0477b3a
SardorNorbekov <sardor.pnu@gmail.com>
2024-02-19 22:45:48 +0900

    first comment

---
+++ #1 homeWork/home.js
@@ -0,0 +1,39 @@
+// 1 Juft va toq sonni topish
+
+/* var num  = 12
+if(num%2 == 0){
+    console.log(`${num} juft son`);
+} else{
+console.log(`${num} toq son`);
+} */
+
+/////////////////////////////////////
+// 2. Ball topish
+
+/* var ball = 45
+
+if(ball >= 98){
+    console.log('A');
+}if( ball >= 85){
+    console.log('B');
+}if( ball >= 60){
+    console.log('C');
+}if(ball <= 45){
+    console.log('Faild');
+} */
+
+/////////////////////////////////////////////
+
+// 3.Meva narxini topish
+
+/* var meva = 'pame'
+
+if (meva === 'olma'){
+    console.log('10 000');
+}if (meva === 'nok'){
+    console.log('12 000');
+}if(meva === 'tarvuz'){
+    console.log('20 000');
+}if(meva !== 'olma' || meva !== ' nok' || meva !== 'tarvuz'){
+    console.log('Bunday meva yo\'q');
+}  */
