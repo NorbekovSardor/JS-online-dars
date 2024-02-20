339cd0f06464e600bbc6afa6b8a4cfc32d79f470
SardorNorbekov <sardor.pnu@gmail.com>
2024-02-20 22:16:02 +0900

    newLesson

---
+++ #2 lesson/main.js
@@ -0,0 +1,184 @@
+// Hometask
+// <<<<< 1 >>>>>>
+// let num = 313;
+
+// num % 2 === 0 && console.log('Juft son');
+// num % 2 !== 0 && console.log('Toq son');
+
+// <<<< 2 >>>>>
+
+// let ball = 45;
+
+// ball >= 86 && ball <= 100 && console.log('A');
+// ball >= 61 && ball <= 85 && console.log('B');
+// ball >= 50 && ball <= 60 && console.log('C');
+// ball < 50 && console.log('Failed');
+
+// <<<<< 3 >>>>
+
+// let meva = 'anor';
+
+// meva === 'olma' && console.log(10000);
+// meva === 'nok' && console.log(12000);
+// meva === 'tarvuz' && console.log(15000);
+// meva !== 'olma' && meva !== 'nok' && meva !== 'tarvuz' && console.log('Bunday meva yuq!');
+
+///////////////////////////////////////////////////////
+
+// If else
+
+// let meva = 'olma';
+
+// if (meva === 'olma') {
+//     console.log(5000);
+// } else if (meva === 'olma') {
+//     console.log(15000);
+// } else if (meva === 'olma') {
+//     console.log(20000);
+// }
+
+// meva === 'olma' && console.log(5000);
+// meva === 'olma' && console.log(15000);
+// meva === 'olma' && console.log(20000);
+
+//////////////////////////////////////////////
+
+// let fruit = 'nok';
+
+// if (fruit === 'nok') {
+//     console.log('10000');
+// }
+
+// if (fruit === 'nok') {
+//     console.log('150000');
+// }
+
+// if (fruit === 'nok') {
+//     console.log(20000);
+// }
+
+///////////////////////////////////////////////
+
+// nested if else
+
+// let meva = 'olma';
+// let kg = 3;
+
+// if (meva === 'olma') {
+    
+//     if (kg === 2) {
+//         console.log('20000');
+//     } else if (kg === 1) {
+//         console.log(10000);
+//     } else {
+//         console.log(5000);
+//     }
+    
+// } else {
+//     console.log('Bunday meva yuq!');
+// }
+
+
+////////////////////////////////////////////
+
+// order is important!!!
+
+// let age = 4;
+
+// if (age < 5) {
+//     console.log('Sen xali yosh bolasan!');
+// } else if (age < 18) {
+//     console.log('Mashina xayday olmaydi!');
+// } else if (age >= 18) {
+//     console.log('MAshina xayday oladi!');
+// }
+
+///////////////////////////////////////////////////
+
+// if with logical &&
+
+// let age = 15;
+// let mashinasiBor = true;
+
+// if (age >= 16 && mashinasiBor) {
+//     console.log('John yoshi yetarli va mashinasi bor!');
+// } else {
+//     console.log('John yoshi kichkina va mashinasi yuq!');
+// }
+
+
+//////////////////////////////////////////////////
+
+// if else with logical ||
+
+// let login = 'hey';
+// let pw = 1234;
+
+// if (login === 'hey' || pw === 123) {
+//     console.log('You are welcome!');
+// } else {
+//     console.log('Pw yoki login xato!');
+// }
+
+//////////////////////////////////////////////////
+
+// if else with logical not
+
+// let check = false;
+
+// if (!check) {
+//     console.log('Salom');
+// } else {
+//     console.log('Xayr');
+// }
+
+
+////////////////////////////////////////////////////////
+
+// switch / case
+
+// let kun = 7;
+
+// switch (kun) {
+//     case 1: console.log('Dushanba'); break
+//     case 2: console.log('Seshanba'); break
+//     case 3: console.log('Chorshanba'); break
+//     case 4: console.log('Payshanba'); break
+//     case 5: console.log('Juma'); break
+//     case 6: console.log('shanba'); break
+//     case 7: console.log('Yakshanba'); break
+//     default : console.log('Bunday xafta kuni yuq!');
+// }
+
+// switch /case => malumotni tekshirganda strict checking(===) qiladi
+// if / else => malumotni tekshirganda strict checking(===) xam not strick checking(==) xolatidayam ishlaydi
+
+///////////////////////////////////////////////////
+
+// ternary operator
+
+// let color = 'yellow';
+
+// color === 'red' ? console.log('Togri') : console.log('notugri');
+
+///////////////////////
+
+// let num = 132;
+
+// num % 2 === 0 ? console.log('Juft son') : console.log('Toq son');
+
+//////////////////////
+
+// let age = 15;
+// let citizen = age >= 18 ? 'Ovoz bera oladi!' : 'Ovoz bera olmaydi!';
+
+// console.log(citizen);
+
+
+// task
+
+// let num = 45;
+
+// terminal => 3 ga bulinadi!
+// terminal => 5 ga bulinadi
+// terminal => 3 ga va 5 ga bulinadi
