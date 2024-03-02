55480fcf55c073ae85289040a15eba4e4e4839da
SardorNorbekov <sardor.pnu@gmail.com>
2024-03-02 22:11:37 +0900

    HomeWork "string"

---
+++ #6 lesson-5/homeWork.js
@@ -0,0 +1,61 @@
+// 1-task agar strgn oxiri l bn tugas true chiqar
+
+/* let str1 =  '  FoziL     '
+
+let str2 = str1.trim().endsWith('L')
+console.log(str2); */
+
+
+// <<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<>>>>>>>>>>>
+
+// 2-task o'zgaruvchini qiymatini almashtirish
+
+/* let str1 = 'Web'
+let str2 =  'Brain'
+
+let str3 = str1.replace('Web', 'Brain')
+let str4 = str1.replace('Brain','Web')
+console.log(str3, str4); */
+
+/* let str1 = 'Web';
+let str2 =  'Brain';
+str1 = str1.replace(/Web/, 'Brain');
+str2 = str2.replace(/Brain/, 'Web');
+
+console.log(`str1-${str1}   str2 - ${str2}`);
+
+ */
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// 3-task
+
+/* let yil = 2017;
+let kabisa = (yil % 4 === 0 );
+
+if (kabisa) {
+    console.log(yil + " kabisa yili");
+} else {
+    console.log(yil + " kabisa yili emas");
+} */
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// 4-task #ni ()ga aylantirish
+
+/* let str1 = '#Fozil#web#brain#';
+let result = str1.replace(/#([^#]+)#/g, '($1)');
+console.log(result);
+
+ */
+
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
+
+// 5-task so'zni 10taga ko'paytir, va web so'zini IT ga o'zgartir
+
+/* let str1 = 'webbrin ';
+let str2 = str1.repeat(10);
+
+str2 = str2.replace(/web/g, 'IT');
+
+console.log(str2);
+ */
