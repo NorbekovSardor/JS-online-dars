33676b05181762163160edeef690fb356e06ecf0
SardorNorbekov <sardor.pnu@gmail.com>
2024-03-06 05:50:25 +0900

    function

---
+++ #7 leson-6/tasks.js
@@ -0,0 +1,52 @@
+// <<<<<<<<<<<<<<<<1-task> pow >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+/* function myMathPow(son, daraja) {
+return son **daraja
+}
+console.log(myMathPow(2,4)) */
+
+//<<<<<<<<<<<<<< 2-task abs() >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+/* function myAbs(son){
+    if(son < 0){
+        return -son
+    }else{
+       return son
+    }
+}
+console.log(myAbs(5)); */
+
+// <<<<<<<<<<< 3-task floor()>>>>>>>>>>>>>>>>>  
+
+/* function myFloor(number) {
+    return number >= 0 ? number | 0 : (number | 0) - 1;
+}
+
+console.log(myFloor(5.7)); // Natija: 5
+console.log(myFloor(-3.4)); // Natija: -4
+ */
+
+//<<<<<<<<<<<<<4-task sqrt()>>>>>>>>>>.>>>>>>>
+
+/* function mySqrt(son){
+return son **0.5
+}
+console.log(mySqrt(81)); */
+
+//<<<<<<<<<<<<<<5-task cbrt>>>>>>>>>>>>>>>>>>>>>>>>>
+/* 
+function myCbrt(number) {
+    return Math.pow(number, 1/3);
+}
+
+let son = 27;
+let son1 = myCbrt(son);
+console.log(son1); 
+ */
+
+//<<<<<<<<<<<< 6-task ceil()>>>>>>>>>>>>>>>>>>>>>>
+/* function myCeil(number) {
+    return (number | 0) + (number === (number | 0) ? 0 : 1);
+}
+
+console.log(myCeil(5.7)); 
+console.log(myCeil(-3.4)); */
