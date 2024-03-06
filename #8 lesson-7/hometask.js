9f8146b3141d60a6c0847308b19fe15b52d2b9d6
SardorNorbekov <sardor.pnu@gmail.com>
2024-03-07 02:44:23 +0900

    Object

---
+++ #8 lesson-7/hometask.js
@@ -0,0 +1,65 @@
+// 1-task user nomli uzgaruvchi ochish kk va name = John, surname = Smith,va nameni "pete"ga
+//o'zgartir va nameni o'chirish kk 
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>></>
+
+
+/* let  user = {
+    name:'John',
+    surname : 'Smith',
+}
+user.name = 'Pete'
+delete user.name
+console.log(user); */
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+//task-2 Agar obj ichida malumot bo'lsa true yo'q bo'lsa false chiqar
+
+/* let schedule1 = {
+    name: 'Sardor'
+};
+let schedule2 = {};
+
+let result1 = Object.keys(schedule1).length > 0;
+let result2 = Object.keys(schedule2).length > 0;
+
+console.log(result1); 
+console.log(result2); 
+ */
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// task-3 obj ichidagi qiymatlar yig'indisini xisoblash kk
+
+// let salaries ={
+//     John: 100,
+//     Ann : 160,
+//     Pete : 130
+// }
+// let sam = salaries.John + (salaries.Ann + salaries.Pete)
+// console.log(sam);
+
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// 4-task obj ichida raqamlar bo'lsa 2ga ko'paytir 
+
+// function multiplyNumeric(obj) {
+//     for (let key in obj) {
+//         if (typeof obj[key] === 'number') {
+//             obj[key] *= 2;
+//         }
+//     }
+// }
+// let menu = {
+//     width: 200,
+//     height: 300,
+//     title: "my menu"
+// };
+// multiplyNumeric(menu);
+// console.log(menu);
+
+
+/* 
+ */
