2413f16110081b1ba8a7b2b6f68ad8d686b20721
SardorNorbekov <sardor.pnu@gmail.com>
2024-03-12 10:41:13 +0900

    Array 1

---
+++ #10 lesson-9/home.js
@@ -0,0 +1,35 @@
+ //1-task ismni search qilish
+
+// let arr1 = [
+//     {id:1, name: 'Sardor'},
+//     {id:2, name: 'Odil'},
+//     {id:3, name: 'Fozil'},
+//     {id:4, name: 'Elyor'},
+// ];
+
+// function onSearch(text) {
+//     for (let i = 0; i < arr1.length; i++) {
+//         if (arr1[i].name.includes(text)) {
+//             console.log(arr1[i].name);
+//         }
+//     }
+// }
+
+// onSearch("S");
+ 
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>......
+
+//ilk  qiymati bir xil qiynatni topish
+// const str = "kKkJLLLH";
+// let arr = [];
+
+// for (let i = 0; i < str.length; i++) {
+//     if (arr.includes(str[i])) {
+//         console.log(str[i]);
+//         break;
+//     }
+//     arr.push(str[i]);
+// }
+
+
