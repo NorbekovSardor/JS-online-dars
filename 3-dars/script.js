675945c733b5c7f9438bcc29f5a99b2a70c2f748
SardorNorbekov <sardor.pnu@gmail.com>
2024-03-17 22:56:31 +0900

    homeWork-3

--- 3-dars/script.js
+++ 3-dars/script.js
@@ -1,75 +1,66 @@
+// 1-task toq va juft sonlarga ajraish
+/* for(let i = 0; i <= 10; i++){
+    
+    if(i % 2 === 0){
+        console.log(i , 'juft son');
+    }else{
+        console.log(i,'Toq son');
+    }
+} */
+//////////////////////////////////////////////
 
-//1-task ball chiqarich( switch,case)
-/* var ball = 60;
+// 2-task harflar sonini topish
+/* let harf = 'Norbekov SardOr oO'
+let count = 0;
+for(let i = 0; i < harf.length; i++){
+    if(harf[i] === 'o' || harf[i] === 'O'){
+    count++;
+    }
+}
+console.log(count); */
 
-switch (true) {
-    case ball >= 98:
-      console.log('A');
-        break;
-    case ball >= 85:
-        console.log('B');
-        break;
-    case ball >= 60:
-        console.log('C');
-        break;
-    default:
-       console.log('Failed');
+///////////////////////////////////////////////////
+
+//3-task so'zni teskari o'girish
+/* let ism = 'Sardor';
+for(let i = ism.length; i >= 0;  i-- ){
+    console.log(ism[i]);
 }
  */
-/////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
 
-// 2-task meva narxini chiqarish (switch, case)
+// 4-task katta harflarni yig'ish
+/* 
+let str = "SarDor";
+let kattaHarf = [];
 
-/* let meva = 'nok'
-switch(true){
-    case meva ==='olma':
-        console.log('12 000');
-        break;
-    case meva ==='nok':
-         console.log('15 000');
-         break;
-     case meva ==='tarvuz':
-         console.log('20  000');
-         default:
-            console.log('bunday meva yo\'q');
+for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i].toUpperCase() && str[i] !== str[i].toLowerCase()) {
+        kattaHarf.push(str[i]);
+    }
 }
+console.log("Katta harflar: " + kattaHarf);
  */
+ 
+/* let str = 'SaRdOR'
+let kattaHarf = []
+ for(let i = 0; i < str.length; i++){
+   if(str[i] === str[i].toUpperCase() && str[i] !== str[i].toLowerCase()){
+    kattaHarf.push(str[i])
 
-//3-task berilgan son a ni 3, 5 yoki ularning hammasiga bo'linishini tekshirish.
+   }
+ }
+ console.log(kattaHarf); */
 
-/* let a = 10;
+ let color = 'blue';
 
- if (a % 3 == 0 && a % 5 == 0){
-    console.log(`'${a}' 3 ga va 5 ga bo'linadi`);
-}else if (a % 3 == 0) {
-    console.log(`'${a}' 3 ga bo'linadi`);
-} else if (a % 5 == 0) {
-    console.log(`5 ga bo'linadi`);
+switch (color) {
+  case 'red':
+    console.log('The color is red');
+    break;
+  case 'blue':
+    console.log('The color is blue');
+    break;
+  default:
+    console.log('The color is neither red nor blue');
 }
- */
-
-////////////////////////////////////////////////
-
-//4-task  'switch'dan 'if else'ga o'zgartirish
-
-
-/* let browser ='Chorme'
-if(browser === 'Edge'){
-console.log('You\' ve got the Edge!');
-}else if(browser ==='Chorme' || browser === 'Firefox' ||  browser === 'Safari' ||  browser === 'Opera'){
-    console.log('Okay we suport these browsers too');
-}else{
-    console.log('We hope that teis page looks ok!');
-}
- *///
-
-////////////////////////////////////////////////////////////////////
-
-//5-task ternary operatorga(?) o'zgartirish
-
-/* let login = ''
-let message = (login === 'Employee')? 'Hello':
-              (login === 'Director')? 'Greetings':
-              (login === '')? 'No login':
-              ''
-              console.log(message); */
