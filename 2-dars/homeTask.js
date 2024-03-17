6c8f3c36fdfd1dbf4052f26259c82a37006de396
SardorNorbekov <sardor.pnu@gmail.com>
2024-03-17 22:54:42 +0900

    homeWork-2

---
+++ 2-dars/homeTask.js
@@ -0,0 +1,75 @@
+
+//1-task ball chiqarich( switch,case)
+/* var ball = 60;
+
+switch (true) {
+    case ball >= 98:
+      console.log('A');
+        break;
+    case ball >= 85:
+        console.log('B');
+        break;
+    case ball >= 60:
+        console.log('C');
+        break;
+    default:
+       console.log('Failed');
+}
+ */
+/////////////////////////////////////////////////////////////////////////////
+
+// 2-task meva narxini chiqarish (switch, case)
+
+/* let meva = 'nok'
+switch(true){
+    case meva ==='olma':
+        console.log('12 000');
+        break;
+    case meva ==='nok':
+         console.log('15 000');
+         break;
+     case meva ==='tarvuz':
+         console.log('20  000');
+         default:
+            console.log('bunday meva yo\'q');
+}
+ */
+
+//3-task berilgan son a ni 3, 5 yoki ularning hammasiga bo'linishini tekshirish.
+
+/* let a = 10;
+
+ if (a % 3 == 0 && a % 5 == 0){
+    console.log(`'${a}' 3 ga va 5 ga bo'linadi`);
+}else if (a % 3 == 0) {
+    console.log(`'${a}' 3 ga bo'linadi`);
+} else if (a % 5 == 0) {
+    console.log(`5 ga bo'linadi`);
+}
+ */
+
+////////////////////////////////////////////////
+
+//4-task  'switch'dan 'if else'ga o'zgartirish
+
+
+/* let browser ='Chorme'
+if(browser === 'Edge'){
+console.log('You\' ve got the Edge!');
+}else if(browser ==='Chorme' || browser === 'Firefox' ||  browser === 'Safari' ||  browser === 'Opera'){
+    console.log('Okay we suport these browsers too');
+}else{
+    console.log('We hope that teis page looks ok!');
+}
+ *///
+
+////////////////////////////////////////////////////////////////////
+
+//5-task ternary operatorga(?) o'zgartirish
+
+/* let login = ''
+let message = (login === 'Employee')? 'Hello':
+              (login === 'Director')? 'Greetings':
+              (login === '')? 'No login':
+              ''
+              console.log(message); */
