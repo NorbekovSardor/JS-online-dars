01a29339c53916873b53215984b9ecfff6188c84
SardorNorbekov <sardor.pnu@gmail.com>
2024-03-17 22:50:03 +0900

    1-homeTask

---
+++ 1-dars/home.js
@@ -0,0 +1,41 @@
+// 1task Juft va toq sonni topish
+
+/* var num  = 12
+if(num%2 == 0){
+    console.log(`${num} juft son`);
+} else{
+console.log(`${num} toq son`);
+} */
+
+/////////////////////////////////////
+// 2task. Ball topish
+
+/* var ball = 45
+
+if(ball >= 98){
+    console.log('A');
+}if( ball >= 85){
+    console.log('B');
+}if( ball >= 60){
+    console.log('C');
+}if(ball <= 45){
+    console.log('Faild');
+} */
+
+/////////////////////////////////////////////
+
+// 3 task .Meva narxini topish
+
+/* var meva = 'f'
+
+if (meva === 'olma'){
+    console.log('10 000');
+}else if (meva === 'nok'){
+    console.log('12 000');
+}else if(meva === 'tarvuz'){
+    console.log('20 000');
+}else {
+    console.log('bunday meva yo\'q');
+}
+ */
+//
